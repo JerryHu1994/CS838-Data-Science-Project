3fa270b7de74d341628cce0ef2cae72e5060e147
Yaqi Zhang <zhang623@wisc.edu>
2018-02-17 13:36:06 -0600

    update

--- data/data-analysis/statistic.py
+++ data/data-analysis/statistic.py
@@ -7,15 +7,8 @@ from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-if __name__ == "__main__":
+def words_count(names):
     '''analyze the number of words in names'''
-    if len(sys.argv) != 2:
-        print("Usage: python %s <namefile>" % (sys.argv[0]))
-        sys.exit(1)
-    filename  = sys.argv[1]
-    with open(filename) as f:
-        names = f.readlines()
     sizes = [len(item.split()) for item in names]
     n = len(sizes)
     counts = Counter(sizes)
@@ -24,3 +17,25 @@ if __name__ == "__main__":
         print("{:1d}: {:6d}\t{:7.6f}".format(item[0], item[1], item[1]/n*100.0))
     plt.hist(np.array(sizes))
     plt.show()
+
+
+def analyze_familynames(names):
+    surname_lst = []
+    for name in names:
+        lst = name.split()
+        if len(lst) != 1:
+            surname_lst.append(lst[-1])
+    counts = Counter(surname_lst)
+    for item in counts.items():
+        print("{}: {}".format(*item))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python %s <namefile>" % (sys.argv[0]))
+        sys.exit(1)
+    filename  = sys.argv[1]
+    with open(filename) as f:
+        names = f.readlines()
+    # words_count(names)
+    analyze_familynames(names)
