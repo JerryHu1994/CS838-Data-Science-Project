ebf34b0355c7cc903c04b24a17be03257acc3b71
Jieru Hu <hjr01211@gmail.com>
2018-02-19 15:41:45 -0600

    Add function to generate negative feature vector, need further test

--- tools/features/splitData.py
+++ tools/features/splitData.py
@@ -15,9 +15,14 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-# define set for prefix and end strings
-endDict = set(['Jr', 'Sr', '\'s'])
+# define set for certain prefix strings
 prefixDict = set(['Mr', 'Ms'])
+
+# define set for certain end strings
+endDict = set(['Jr', 'Sr', '\'s'])
+
+# define a list contains the all possible length of negative examples
+negLengthList = [1, 2, 3]
 
 # define string for sentence breaking
 delimiter = '[,.!?;]\s*'
@@ -43,12 +48,22 @@ def checkPrefixMatch(linelist, ind):
             return '1'
     return '0'
 
+# check if the startIndex and endIdx is contained within a positive name
+def inPositive(nameDict, startI, endI):
+    for name in nameDict:
+        if name[0] <= startI and endI <= name[1]:
+            return True
+    return False
+
+# Returns if the given sentence string contains a name tag
+def containsName(line):
+   return True if "<person>" in line else False
 
 # Returns a list of positive samples, given a sentence string
 def extractPositiveFeature(line):
     featureList = []
     names = re.findall(r"<person>[^<]*</person>", line)
-    
+
     # replace <person>name</person> with <person> for the easieness of index counting
     replacedLine = re.sub(r"<person>[^<]*</person>", '<person>', line)
     replacedLineList = replacedLine.split()
@@ -56,13 +71,13 @@ def extractPositiveFeature(line):
     nameIdxlist = [ind for ind, val in enumerate(replacedLineList) if val == '<person>']
 
     #print (replacedLine)
-    tempLine = line.replace('<person>', ' ').replace('</person>', ' ')
-    preCount = 0
+    preCount = 0 # preCount stores the difference between the length of the replacedLineList and it original
+    list
     for ind, name in enumerate(names):
         nameStr = name[8:-9] # save the name string
         lengthStr = str(len(nameStr.split())) # save the length string
         capitalizedStr = checkCapitalized(nameStr) # save the capitalized boolean string
-        
+
         headPositionStr = str(nameIdxlist[ind] + preCount) # save the position from head
         preCount += int(lengthStr) - 1
         lineLengthStr = str(len((line.replace('<person>', '').replace('</person>', '').split()))) # save the sentence length string
@@ -73,17 +88,53 @@ def extractPositiveFeature(line):
         features = [nameStr, lengthStr, capitalizedStr, headPositionStr, lineLengthStr, endStr, prefixStr]
         featureStr = ', '.join(features)
 
-        #print (featureStr)
         featureList.append(featureStr)
     return featureList
 
 # Returns a list of negative samples, given a sentence string
-def extractNegativeFeature():
-    return
+def extractNegativeFeature(line):
+    nameIdxDict = [] # stores a list of [startIndex, endIndex] set for each labeled name
+    names = re.findall(r"<person>[^<]*</person>", line)
+    # replace <person>name</person> with <person> for the easieness of index counting
+    replacedLine = re.sub(r"<person>[^<]*</person>", '<person>', line)
+    replacedLineList = replacedLine.split()
+    # save all the indexes of matched names
+    nameIdxlist = [ind for ind, val in enumerate(replacedLineList) if val == '<person>']
+    preCount = 0 # preCount stores the difference between the length of the replacedLineList and it original
+   
+    # save all tagged name index pairs
+    for ind, name in enumerate(names):
+        nameStr = name[8:-9] # save the name string
+        currLen = len(nameStr.split())
+        nameIdxDict.append((nameIdxlist[ind] + preCount, nameIdxlist[ind]+ preCount + currLen))
+        preCount += currLen - 1
+    
+    wordList = line.replace('<person>', '').replace('</person>', '').split()
+    featureList = []
+    # loop through different negative lengths
+    for l in negLengthList:
+        for start in range(0, len(wordList)-l+1):
+            startI, endI = start, start+l
+            # skip if the currPair is contained by a tagged name
+            if inPositive(nameIdxDict, startI, endI): continue
+        
+            # start producing feature vectors
+            nameStr = ' '.join(wordList[startI:endI])#' '.join(i) for i in wordList[startI:endI] # save the name string
+            lengthStr = str(l) # save the length string
+            capitalizedStr = checkCapitalized(nameStr) # save the capitalized boolean string
 
-# Returns if the given sentence string contains a name tag
-def containsName(line):
-   return True if "<person>" in line else False
+            headPositionStr = str(startI) # save the position from head
+            lineLengthStr = str(len(wordList)) # save the sentence length string
+
+            endStr = checkEndMatch(wordList, endI-1) # save the end-pattern boolean string
+            prefixStr = checkPrefixMatch(wordList, startI) # save the prefix-pattern boolean string
+
+            features = [nameStr, lengthStr, capitalizedStr, headPositionStr, lineLengthStr, endStr, prefixStr]
+            featureStr = ', '.join(features)
+
+            featureList.append(featureStr)
+
+    return featureList
 
 # main method
 def main():
@@ -95,14 +146,13 @@ def main():
         print ("[USAGE]: python splitData.py input_dir output_file pos/neg\n")
     dataFlag = True if dataFlag == "pos" else False
 
-
     inputFiles = []
     if os.path.exists(inputDir):
         inputFiles = os.listdir(inputDir)
     else:
         print ("[ERROR]: Input directory not exists")
     outputFile = open(outputDir, 'w')
-    
+
     #loop through all labeled files
     for fname in inputFiles:
         filePath = inputDir + "/" + fname
@@ -113,7 +163,7 @@ def main():
             lineList = re.split(delimiter, lines)
             #split the file contents into strings using certain delimiters
             for line in lineList:
-                #print (line)
+                print (line)
                 if dataFlag:
                     if containsName(line):
                         # write all the positive feature vectors into the output file
@@ -121,14 +171,12 @@ def main():
                         for feature in featureList:
                             outputFile.write(feature + "\n")
                 else:
-                    if not containsName(line):
-                        # write all the negative feature vectors into the output file
-                        featureList = extractNegativeFeature(line)
-                        for feature in featureList:
-                            outputFile.write(feature + "\n")
+                    # write all the negative feature vectors into the output file
+                    featureList = extractNegativeFeature(line)
+                    for feature in featureList:
+                        outputFile.write(feature + "\n")
     # close the output file
     outputFile.close()
-
 
 if __name__ == "__main__":
   main()
