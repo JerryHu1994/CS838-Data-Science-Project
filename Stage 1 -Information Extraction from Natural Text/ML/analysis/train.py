a69d1f7f7d5422ed39efe62e21812bdcc9c769a0
Jieru Hu <hjr01211@gmail.com>
2018-02-24 10:44:31 -0600

    restructure folder

--- Stage 1 -Information Extraction from Natural Text/ML/analysis/train.py
+++ Stage 1 -Information Extraction from Natural Text/ML/analysis/train.py
@@ -6,6 +6,8 @@ Yaqi Zhang
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import tree
+from sklearn import svm
+from sklearn import linear_model
 
 def read_file(filename):
     with open(filename, 'r') as f:
@@ -20,20 +22,22 @@ def read_file(filename):
     y = M[:, -1:]
     return x, y
 
-if __name__ == "__main__":
+def train_and_test(clf, name):
     train_x_positive, train_y_positive = read_file("positive.dat")
     train_x_negative, train_y_negative = read_file("negative_pruned.dat")
     train_n_positive = train_x_positive.shape[0] # number of positive samples
     train_x = np.vstack((train_x_positive, train_x_negative))
     train_y = np.vstack((train_y_positive, train_y_negative))
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(train_x, train_y)
+    clf.fit(train_x, train_y.ravel())
     train_positive_predict = clf.predict(train_x_positive)
     nTP = train_positive_predict.sum() # true positive
     train_predict = clf.predict(train_x)
     nP = train_predict.sum() # positive
-    print("train recall = {:0.6f} %".format(nTP / train_n_positive * 100.0))
-    print("train presison = {:0.6f} %".format(nTP / nP * 100.0))
+    train_recall, train_precision = nTP / train_n_positive * 100.0, nTP / nP * 100.0
+    print(name, "train recall = {:0.6f} %".format(train_recall))
+    print(name, "train presison = {:0.6f} %".format(train_precision))
+    print(name, "train F1-score = {:0.6f} %".format(2 * (train_precision * train_recall) / (train_precision
+    + train_recall)))
     test_x_positive, test_y_positive = read_file("test_positive.dat")
     test_x_negative, test_y_negative = read_file("test_negative_pruned.dat")
     test_x = np.vstack((test_x_positive, test_x_negative))
@@ -41,5 +45,22 @@ if __name__ == "__main__":
     test_positive_predict = clf.predict(test_x_positive)
     nP = test_predict.sum()
     nTP = test_positive_predict.sum()
-    print("test recall = {:0.6f} %".format(nTP / test_x_positive.shape[0] * 100.0))
-    print("test precision = {:0.6f} %".format(nTP / nP * 100.0))
+    test_recall, test_precision = nTP / test_x_positive.shape[0] * 100.0, nTP / nP * 100.0
+    print("=====================================================")
+    print(name, "test recall = {:0.6f} %".format(test_recall))
+    print(name, "test precision = {:0.6f} %".format(test_precision))
+    print(name, "test F1-score = {:0.6f} %".format(2 * (test_precision * test_recall) / (test_precision +
+    test_recall)))
+    print ("\n")
+
+if __name__ == "__main__":
+    
+    # use decision tree classifier
+    dt_clf = tree.DecisionTreeClassifier()
+    train_and_test(dt_clf, "decision_tree")
+    # use svm classifier
+    svm_clf = svm.SVC()
+    train_and_test(svm_clf, "SVM")
+    # use logisticRegression
+    lr_clf = linear_model.LogisticRegression()
+    train_and_test(lr_clf, "logistic_regression")
--- Stage 1 -Information Extraction from Natural Text/tools/feature_generator/splitTaggedText.py
+++ Stage 1 -Information Extraction from Natural Text/tools/feature_generator/splitTaggedText.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ##################################
 # University of Wisconsin-Madison
 # Author: Jieru Hu
