67de99da94c8aa5de771b1c0dc3141d2dc43b953
Jieru Hu <hjr01211@gmail.com>
2018-02-24 21:08:52 -0600

    Add cross-validation to the ML classifier

--- Stage 1 -Information Extraction from Natural Text/ML/analysis/train.py
+++ Stage 1 -Information Extraction from Natural Text/ML/analysis/train.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
 Univertity of Wisconsin-Madison
-Yaqi Zhang
+Yaqi Zhang, Jieru Hu
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn import svm
 from sklearn import linear_model
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
+
 
 def read_file(filename):
     with open(filename, 'r') as f:
@@ -22,13 +27,42 @@ def read_file(filename):
     y = M[:, -1:]
     return x, y
 
+# cross-validation on the train set
+def cv(clf, name):
+    train_x_positive, train_y_positive = read_file("positive.dat")
+    train_x_negative, train_y_negative = read_file("negative_pruned.dat")
+    train_n_positive = train_x_positive.shape[0] # number of positive samples
+    train_x = np.vstack((train_x_positive, train_x_negative))
+    train_y = np.vstack((train_y_positive, train_y_negative))
+    # shuffle the positive and negative sets
+    shuffled_x, shuffled_y = shuffle(train_x, train_y, random_state=0)
+    kf = KFold(n_splits=4)
+    nTP, nP = 0, 0
+    for train_index, test_index in kf.split(shuffled_x):
+        kf_x_train, kf_x_test = shuffled_x[train_index], shuffled_x[test_index]
+        kf_y_train, kf_y_test = shuffled_y[train_index], shuffled_y[test_index]
+        #train the classifier
+        clf.fit(kf_x_train, kf_y_train.ravel())
+        test_predict = clf.predict(kf_x_test)
+        nP += test_predict.sum()
+        # extract text_positives
+        kf_x_test_positive = kf_x_test[np.where(kf_y_test==1)[0]]
+        test_predict_positive = clf.predict(kf_x_test_positive)
+        nTP += test_predict_positive.sum()
+        
+    train_precision = nTP / nP * 100.0
+    print(name, "Cross-Validation presison = {:0.6f} %".format(train_precision))
+    return train_precision
+
 def train_and_test(clf, name):
     train_x_positive, train_y_positive = read_file("positive.dat")
     train_x_negative, train_y_negative = read_file("negative_pruned.dat")
     train_n_positive = train_x_positive.shape[0] # number of positive samples
     train_x = np.vstack((train_x_positive, train_x_negative))
     train_y = np.vstack((train_y_positive, train_y_negative))
-    clf.fit(train_x, train_y.ravel())
+    # shuffle the positive and negative sets
+    shuffled_x, shuffled_y = shuffle(train_x, train_y, random_state=0)
+    clf.fit(shuffled_x, shuffled_y.ravel())
     train_positive_predict = clf.predict(train_x_positive)
     nTP = train_positive_predict.sum() # true positive
     train_predict = clf.predict(train_x)
@@ -57,10 +91,22 @@ if __name__ == "__main__":
     
     # use decision tree classifier
     dt_clf = tree.DecisionTreeClassifier()
-    train_and_test(dt_clf, "decision_tree")
     # use svm classifier
     svm_clf = svm.SVC()
-    train_and_test(svm_clf, "SVM")
     # use logisticRegression
-    lr_clf = linear_model.LogisticRegression()
-    train_and_test(lr_clf, "logistic_regression")
+    lor_clf = linear_model.LogisticRegression()
+    rf_clf = RandomForestClassifier(max_depth=2, random_state=0)
+    lir_clf = linear_model.LinearRegression()
+    clf_list = [dt_clf, svm_clf, lor_clf, rf_clf, lir_clf]
+    clf_name = ["Decision Tree", "SVM", "Logistic Regression", "Random Forest", "Linear Regression"]
+    precisions = []
+    for ind, clf in enumerate(clf_list):
+        precisions.append(cv(clf, clf_name[ind]))
+    max_precision = max(precisions)
+    max_ind = precisions.index(max_precision)
+    best_clf, best_clf_name = clf_list[max_ind], clf_name[max_ind]
+    print ("\n")
+    print (best_clf_name, "has the best cross-validation precision equal to {:0.6f} %".format(max_precision))
+    # let's do the real train and test'
+    train_and_test(best_clf, best_clf_name)
+
