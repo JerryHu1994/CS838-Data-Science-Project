7cabd262979762e973cb972067802c2438eda62a
Jieru Hu <hjr01211@gmail.com>
2018-03-10 15:58:47 -0600

    "Some change"

--- Stage-2-Crawling-structured-Web-data/src/cars_com_crawling.py
+++ Stage-2-Crawling-structured-Web-data/src/cars_com_crawling.py
@@ -7,8 +7,6 @@
 
 import urllib.request as urllib2
 from bs4 import BeautifulSoup as bs
-import numpy as np
-import matplotlib.pyplot as plt
 import csv
 import json
 
@@ -34,6 +32,12 @@ def construct_urls():
         url_list.append(url_template%(i+1, cars_per_page))
     return url_list
 
+# extract more car infomation from a bs4.element.Tag object into a python dictionary
+def get_more_info(car_detail):
+
+    car_miles = car_detail.find('span', class_='listing-row__mileage').text
+    car_detail = {"miles": car_miles}
+    return car_detail
 
 # main method
 def main():
@@ -41,19 +45,32 @@ def main():
     f, w = csv_init("cars_com.csv", ["name", "brand", "color", "price", "seller_name", "seller_phone",
             "seller_average_rating", "seller_review_count"])
 
-    # start crawling
+    # start crawling given a list of cars.com urls
     for url in cars_urls:
         with urllib2.urlopen(url) as uopen:
             oururl = uopen.read()
         soup = bs(oururl, 'lxml')
-        cars = json.loads(soup.find('script', type='application/ld+json').text)
-        for car_data in cars:
-            car_dict = {"name": car_data['name'], "brand": car_data['brand']['name'], "color":
+        # get car general information from json script
+        cars_info = json.loads(soup.find('script', type='application/ld+json').text)
+
+        # get more detailed car information from HTML tags
+        cars_detail_list = soup.find_all('div', class_='shop-srp-listings__listing')
+
+        if (len(cars_info) != len(cars_detail_list)):
+            print ("Error the size of car json information and size of car html information does not match")
+            continue
+        # for each car, extract and insert information into csv table
+        for ind, car_data in enumerate(cars_info):
+            car_info = {"name": car_data['name'], "brand": car_data['brand']['name'], "color":
         car_data['color'], "price": car_data['offers']['price'], "seller_name": car_data['offers']['seller']['name'],
         "seller_phone": car_data['offers']['seller']['telephone'], "seller_average_rating":
         car_data['offers']['seller']['aggregateRating']['ratingValue'], "seller_review_count": car_data['offers']['seller']['aggregateRating']['reviewCount']}
-            #print (car_dict)
-            w.writerow(car_dict)
+            car_details = get_more_info(cars_detail_list[ind])
+
+            # combine two dicts
+            car_dict = {**car_info, **car_details}
+            print (car_dict)
+            #w.writerow(car_dict)
         f.close()
         return
 
