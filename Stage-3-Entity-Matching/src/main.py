530024d1769fe4fcc0dea350e355185a40bdb407
Yaqi Zhang <zhang623@wisc.edu>
2018-04-06 14:06:41 -0500

    made some minor style change

--- Stage-3-Entity-Matching/src/main.py
+++ Stage-3-Entity-Matching/src/main.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ##################################
 # University of Wisconsin-Madison
 # Author: Jieru Hu, Yaqi Zhang
 ##################################
+# main program
+##################################
+
 
 import py_entitymatching as em
 import pandas as pd
 
 
-# main method
 def main():
+    """main method"""
     table_a_path = "../data/cars_com_data/Honda-Accord-53715-25-used.csv"
     table_b_path = "../data/market_check_data/Honda-Accord-53715-25-used.csv"
 
@@ -18,5 +21,5 @@ def main():
     print (type(A))
 
 if __name__ == "__main__":
-  main()
+    main()
 
--- Stage-3-Entity-Matching/src/merge_csv.py
+++ Stage-3-Entity-Matching/src/merge_csv.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ##################################
 # University of Wisconsin-Madison
-# Author: Jieru Hu
+# Author: Jieru Hu, Yaqi Zhang
 ##################################
-# This file takes into a list of csv files and merge them into a single csv file.
-import numpy as np
-import pandas as pd
+# This file takes into a list of 
+# csv files and merge them into a 
+# single csv file.
+##################################
+
 import os
 import sys
+import numpy as np
+import pandas as pd
 
 # main method
 def main():
@@ -17,11 +21,13 @@ def main():
     csv_path = sys.argv[2]
     if not os.path.exists(csv_path):
         print ("{} not found".format(csv_path))
-    csv_files = [csv_path+f for f in os.listdir(csv_path)]
+        sys.exit(1)
+    # csv_files = [csv_path+f for f in os.listdir(csv_path)]
+    csv_files = [os.path.join(csv_path, f) for f in os.listdir(csv_path) if f.endswith('.csv')]
     all_data = [pd.read_csv(f) for f in csv_files]
     merged_data = pd.concat(all_data, ignore_index=True)
     merged_data.to_csv(sys.argv[1], encoding='utf-8',index=False)
 
 if __name__ == "__main__":
-  main()
+    main()
 
