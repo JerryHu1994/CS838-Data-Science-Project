34f07779d1ba94487433d5763935370ba89f183c
Yaqi Zhang <zhang623@wisc.edu>
2018-04-08 11:33:40 -0500

    update stage2_pipeline

--- Stage-2-Crawling-structured-Web-data/stage2_pipeline.sh
+++ Stage-2-Crawling-structured-Web-data/stage2_pipeline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#written by Jieru Hu
+##################################
+# University of Wisconsin-Madison
+# Author: Jieru Hu, Yaqi Zhang
+##################################
+# Stage 2 pipeline example
+##################################
 
 make="Honda"
 model="Accord"
@@ -8,9 +13,10 @@ radius=25
 condition="used"
 car_json_file="./src/cars_com_make_model.json"
 marketcheck_key="./src/marketcheck_key"
-outdir="./data/"
+cars_outdir="./data/cars_com_data/"
+market_outdir="./data/market_check_data/"
 echo "Crawling $condition $make-$model from cars.com, in $zipcode with $radius miles"
-python ./src/cars_com_crawling.py $make $model $zipcode $radius $condition $car_json_file $outdir
+python ./src/cars_com_crawling.py $make $model $zipcode $radius $condition $car_json_file $cars_outdir
 
 echo "Crawling $condition $make-$model from marketcheck, in $zipcode with $radius miles"
-python ./src/marketcheck_car_api.py $make $model $zipcode $radius $condition $marketcheck_key $outdir
+python ./src/marketcheck_car_api.py $make $model $zipcode $radius $condition $marketcheck_key $market_outdir
